5982375986e8bf53311322b2706f75a484ef4a2a
Timdows <tim@inexpro.nl>
2018-11-28 12:23:00 +0100

    Implement tasks

--- Program.cs
+++ Program.cs
@@ -1,52 +1,69 @@
-﻿using ImageSharp;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ImageResizer
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			if (args.Length != 2)
-			{
-				Console.WriteLine($"Missing directory and/or maxSize arguments");
-				return;
-			}
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Needed arguments:\r\n1. Input directory\r\n2. Save directory\r\n3. MaxSize");
+                return;
+            }
 
-			var directory = args[0].ToString();
-			if (!int.TryParse(args[1], out var maxSize))
-			{
-				Console.WriteLine($"Could not parse int maxSize");
-			}
+            var inputDirectory = args[0].ToString();
+            var saveDirectory = args[1].ToString();
+            if (!int.TryParse(args[2], out var maxSize))
+            {
+                Console.WriteLine($"Could not parse int maxSize");
+            }
 
-			Console.WriteLine($"Using directory {directory} and maxSize: {maxSize}");
+            Console.WriteLine($"1. Input directory: {inputDirectory}\r\n2. Save directory: {saveDirectory}\r\n3. MaxSize: {maxSize}");
 
-			var files = Directory.GetFiles(directory, "*.jpg", SearchOption.AllDirectories);
-			var current = 0;
+            var files = Directory.GetFiles(inputDirectory, "*.jpg", SearchOption.AllDirectories);
 
-			foreach (var file in files)
-			{
-				Console.WriteLine($"{current++}/{files.Length} - Processing {file}");
-				ScaleImage(file, maxSize);
-			}
-		}
+            List<Task> backgroundTasks = new List<Task>();
+            foreach (var file in files)
+            {
+                // Console.WriteLine($"{current++}/{files.Length} - Processing {file}");
+                // ScaleImage(file, maxSize);
+                var task = Task.Run(() => ScaleImage(file, saveDirectory, maxSize));
+                backgroundTasks.Add(task);
+            }
 
-		public static void ScaleImage(string imagePath, int maxSize)
-		{
-			// Image.Load(string path) is a shortcut for our default type. Other pixel formats use Image.Load<TPixel>(string path))
-			using (Image<Rgba32> image = Image.Load(imagePath))
-			{
-				var ratioX = (double)maxSize / image.Width;
-				var ratioY = (double)maxSize / image.Height;
-				var ratio = Math.Min(ratioX, ratioY);
+            await Task.WhenAll(backgroundTasks);
+        }
 
-				var newWidth = (int)(image.Width * ratio);
-				var newHeight = (int)(image.Height * ratio);
+        public static async Task ScaleImage(string imagePath, string saveDirectory, int maxSize)
+        {
+            var fileName = Path.GetFileName(imagePath);
+            var savePath = Path.Combine(saveDirectory, fileName);
+            var stopwatch = Stopwatch.StartNew();
 
-				image.Resize(newWidth, newHeight)
-					.Save(imagePath); // automatic encoder selected based on extension.
-			}
-		}
-	}
+            using (Image<Rgba32> image = Image.Load(imagePath))
+            {
+                var ratioX = (double)maxSize / image.Width;
+                var ratioY = (double)maxSize / image.Height;
+                var ratio = Math.Min(ratioX, ratioY);
+
+                var newWidth = (int)(image.Width * ratio);
+                var newHeight = (int)(image.Height * ratio);
+
+                image.Mutate(x => x.Resize(newWidth, newHeight));
+                image.Save(savePath);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Done after {stopwatch.ElapsedMilliseconds} with {imagePath}");
+        }
+    }
 }
